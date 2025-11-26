cb0b14b7a579b14ec15f1ae6a078ec6ccca1b9f1
Robert Martens <robert.martens@tetrate.io>
2025-11-25 22:47:41 -0500

    basic

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,62 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/masterfuzz/toysort/pkg"
+)
+
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var fname string
+	if scanner.Scan() {
+		fname = scanner.Text()
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error reading input: %v", err)
+	}
+
+
+	file, err := os.Open(fname)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	top := pkg.NewKVTopN(2)
+	scanner = bufio.NewScanner(file)
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			log.Fatalf("error reading file %v", err)
+		}
+
+		line := scanner.Text()
+
+		top.Push(parseLine(line))
+	}
+
+	sorted := top.TopN()
+	for _, s := range sorted {
+		fmt.Println(s)
+	}
+
+}
+
+func parseLine(line string) pkg.KeyVal {
+	splits := strings.Split(line, " ")
+	v, err := strconv.ParseInt(splits[1], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	return pkg.KeyVal{
+		Key: splits[0],
+		Val: v,
+	}
 }
--- pkg/toysort_test.go
+++ pkg/toysort_test.go
@@ -1,1 +1,43 @@
 package pkg_test
+
+import (
+	"container/heap"
+	"fmt"
+	"testing"
+
+	. "github.com/masterfuzz/toysort/pkg"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHeap(t *testing.T) {
+	h := &KeyValHeap{
+		KeyVal{
+			Key: "a",
+			Val: 10,
+		},
+		KeyVal{
+			Key: "a",
+			Val: 6,
+		},
+	}
+
+	heap.Init(h)
+	heap.Push(h, KeyVal{Val: 5})
+	heap.Push(h, KeyVal{Val: 12})
+
+	assert.Equal(t, (*h)[0].Val, int64(5))
+	assert.Equal(t, (*h)[h.Len()-1].Val, int64(12))
+}
+
+func TestTopN(t *testing.T) {
+	top := NewKVTopN(5)
+	for i := range 20 {
+		top.Push(KeyVal{Key: fmt.Sprintf("%d", i), Val: int64(i)})
+	}
+
+	assert.Equal(t, 5, top.Items.Len())
+	fmt.Printf("%v", top.Items)
+	assert.Equal(t, int64(15), top.Pop().Val)
+
+	assert.Equal(t, []string{"19", "18", "17", "16"}, top.TopN())
+}
